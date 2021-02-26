2bc309ce29b1207b934135c7b54616821f9d5f0b
RavipatiRamya331 <79695226+RavipatiRamya331@users.noreply.github.com>
2021-02-26 15:55:37 +0530

    Update filedecrypt.py

--- filedecrypt.py
+++ filedecrypt.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Created on Fri feb 26:14:09 2020
+@author: RavipatiRamya
+"""
+
+
+
+
+
+
 import os
 from Crypto.Cipher import AES
 from Crypto import Random
